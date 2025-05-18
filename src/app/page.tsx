bc4d2b64887384ceca68878ff54456b484e3ff35
David <topghostly@gmail.com>
2025-05-18 10:58:40 +0100

    Refactor Home component to remove unused imports and clean up commented code in Footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import { Service } from "@/components/page/service-section";
 import { ThemeInfo } from "@/components/page/theme-info";
 import { InfinityScroll } from "@/components/ui/infinity-scroll";
 import { Separator } from "@/components/ui/separator";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import Loader from "@/components/page/loader";
 
 export default function Home() {
@@ -27,7 +27,7 @@ export default function Home() {
 
   return (
     <>
-      {/* <Loader /> */}
+      <Loader />
       {/* <Loader ref={[textRef, containerRef]} /> */}
       <div className=" max-w-[1280px] mx-auto px-3 lg:px-0.5 flex flex-col gap-[0rem] items-center justify-items-center min-h-screen pb-2 pt-12">
         <ThemeInfo />
--- src/components/page/footer-section.tsx
+++ src/components/page/footer-section.tsx
@@ -1,47 +1,47 @@
-import { Copyright, Swords } from "lucide-react";
-import Image from "next/image";
+import { Swords } from "lucide-react";
+// import Image from "next/image";
 import Link from "next/link";
 import { SOCIALS } from "../ui/menu-box";
 
-const InfoHoder = () => {
-  return (
-    <div className="h-[50px] grid grid-cols-[50px_200px] border border-foreground">
-      <div className="grid relative place-content-center h-full border border-r-foreground border-b-foreground w-full">
-        <Image
-          src={"/images/svg/ico-01.svg"}
-          alt="logo svg icon"
-          width={35}
-          height={35}
-        />
-      </div>
-      <div className="grid grid-rows-[0.9fr_1fr]">
-        <div className="border border-b-foreground">
-          <p
-            style={{
-              fontFamily: "var(--font-gobold)",
-              fontSize: "15px",
-              textTransform: "uppercase",
-              textAlign: "center",
-              display: "flex",
-              alignItems: "center",
-              gap: "5px",
-              justifyContent: "center",
-            }}
-          >
-            2025{" "}
-            <span>
-              <Copyright size={15} />
-            </span>{" "}
-            All rights reserved
-          </p>
-        </div>
-        <div className="relative text-[8px] text-center">
-          David is a Full-Stack Developer focused on React, Node.js, and ML.
-        </div>
-      </div>
-    </div>
-  );
-};
+// const InfoHoder = () => {
+//   return (
+//     <div className="h-[50px] grid grid-cols-[50px_200px] border border-foreground">
+//       <div className="grid relative place-content-center h-full border border-r-foreground border-b-foreground w-full">
+//         <Image
+//           src={"/images/svg/ico-01.svg"}
+//           alt="logo svg icon"
+//           width={35}
+//           height={35}
+//         />
+//       </div>
+//       <div className="grid grid-rows-[0.9fr_1fr]">
+//         <div className="border border-b-foreground">
+//           <p
+//             style={{
+//               fontFamily: "var(--font-gobold)",
+//               fontSize: "15px",
+//               textTransform: "uppercase",
+//               textAlign: "center",
+//               display: "flex",
+//               alignItems: "center",
+//               gap: "5px",
+//               justifyContent: "center",
+//             }}
+//           >
+//             2025{" "}
+//             <span>
+//               <Copyright size={15} />
+//             </span>{" "}
+//             All rights reserved
+//           </p>
+//         </div>
+//         <div className="relative text-[8px] text-center">
+//           David is a Full-Stack Developer focused on React, Node.js, and ML.
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 export const Footer: React.FC = () => {
   return (
